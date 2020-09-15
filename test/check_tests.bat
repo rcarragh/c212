c54e2849cb53a0bb1809948e4b82400e7201fde0
Raymond Carragher <rcarragh@gmail.com>
2020-09-15 20:56:42 +0100

    Fix batch file directories

--- test/check_tests.bat
+++ test/check_tests.bat
@@ -1,111 +1,108 @@
-#!/bin/bash
-
-
 echo "#####################################"
 echo "Checking errorcontrol...."
 cd error_control/
 CALL check_test.bat
-cd ..
+cd..
 
 
 echo "#####################################"
-echo "Checking c2121a...."
+echo "Checking c212.1a...."
 
-cd c2121a/changed_parameters 
+cd c212.1a/changed_parameters 
 CALL check_test.bat
-cd ../..
+cd../..
 
 
-cd c2121a/default_parameters/ 
+cd c212.1a/default_parameters/ 
 CALL check_test.bat
-cd ../..
+cd../..
 
-cd c2121a/initial_values/ >/dev/null
+cd c212.1a/initial_values/
 CALL check_test.bat
-cd ../..
+cd../..
 
 
-cd c2121a/single_chain/ >/dev/null
+cd c212.1a/single_chain/
 CALL check_test.bat
-cd ../..
+cd../..
 
 
-cd c2121a/changed_global_params/
+cd c212.1a/changed_global_params/
 CALL check_test.bat
-cd ../..
+cd../..
 
 
 echo "#####################################"
 
 echo "#####################################"
-echo "Checking c212BB...."
+echo "Checking c212.BB...."
 
-cd c212BB/changed_global_sim_params/
+cd c212.BB/changed_global_sim_params/
 CALL check_test.bat
-cd ../..
+cd../..
 
 
-cd c212BB/changed_individual_sim_params/
+cd c212.BB/changed_individual_sim_params/
 CALL check_test.bat
-cd ../..
+cd../..
 
 
-cd c212BB/changed_pm_weights/
+cd c212.BB/changed_pm_weights/
 CALL check_test.bat
-cd ../..
+cd../..
 
 
-cd c212BB/default_parameters/
+cd c212.BB/default_parameters/
 CALL check_test.bat
-cd ../..
+cd../..
 
-cd c212BB/initial_values/
+cd c212.BB/initial_values/
 CALL check_test.bat
-cd ../..
+cd../..
 
-cd c212BB/single_chain/
+cd c212.BB/single_chain/
 CALL check_test.bat
-cd ../..
+cd../..
 
 
 echo "#####################################"
-echo "Checking c2121a.interim...."
+echo "Checking c212.1a.interim...."
 
-cd c2121a.interim/changed_parameters
+cd c212.1a.interim/changed_parameters
 CALL check_test.bat
-cd ../..
+cd../..
 
-cd c2121a.interim/default_parameters
+cd c212.1a.interim/default_parameters
 CALL check_test.bat
-cd ../..
+cd../..
 
-cd c2121a.interim/initial_values/
+cd c212.1a.interim/initial_values/
 CALL check_test.bat
-cd ../..
+cd../..
 
-cd c2121a.interim/single_chain/
+cd c212.1a.interim/single_chain/
 CALL check_test.bat
-cd ../..
+cd../..
 
 echo "#####################################"
-echo "Checking c212BB.interim...."
+echo "Checking c212.BB.interim...."
 
-cd c212BB.interim/changed_pm_weights/
+cd c212.BB.interim/changed_pm_weights/
 CALL check_test.bat
-cd ../..
+cd../..
 
-cd c212BB.interim/changed_sim_params/
+cd c212.BB.interim/changed_sim_params/
 CALL check_test.bat
-cd ../..
+cd../..
 
-cd c212BB.interim/default_parameters/
+cd c212.BB.interim/default_parameters/
 CALL check_test.bat
-cd ../..
+cd../..
 
-cd c212BB.interim/initial_values/
+cd c212.BB.interim/initial_values/
 CALL check_test.bat
-cd ../..
+cd../..
 
-cd /c212BB.interim/single_chain/
+cd /c212.BB.interim/single_chain/
 /CALL check_test.bat
-cd ../..
+cd../..
